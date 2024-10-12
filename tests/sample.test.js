e6246a8a8aac2e8bff6a97cd122afa49529c102e
shashank00818 <shashank00818@gmail.com>
2024-10-13 00:14:56 +0530

    assertion and wait for body added

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -2,6 +2,8 @@ module.exports = {
     "My test" : function(browser){
         browser
         .url("https://www.cricbuzz.com/")
+        .waitForElementVisible('body')
+        .assert.titleContains("Cricbuzz.com")
         .end();
     }
 }
